f0a08ec0de80da04c008e38d4e7846d844133385
I569380 <p.kumar.r@sap.com>
2024-11-11 18:35:39 +0530

    Fixed Bugs

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,63 +1,51 @@
 import React, {useState} from 'react'
 
 
-export default function About() {
-    const [myStyle, setStyle] = useState({
-        color: "black",
-        backgroundColor: "white",
-        border: "2px solid black"
-    });
+export default function About(props) {
+    // const [myStyle, setStyle] = useState({
+    //     color: "black",
+    //     backgroundColor: "white",
+    //     marginTop: "10px"
+    // });
 
-    const [btnText, setBtnText] = useState("Enable Dark Mode");
-
-    const handleDarkMode = () => {
-        console.log("Dark mode has been enabled");
-        // useState("Disable Dark Mode");
-        if(myStyle.color === "white"){
-            setStyle({
-                color: "black",
-                backgroundColor: "white"
-            });
-
-            setBtnText("Enable Dark Mode");
-        }else{
-            setStyle({
-                color: "white",
-                backgroundColor: "black",
-                border: "2px solid white"
-            });
-            setBtnText("Enable Light Mode");
-        }
-
+    let myStyle = {
+        color: props.mode === `dark` ? `white` : `black`,
+        backgroundColor: props.mode === `dark` ? `black` : `white`,
+        border: "2px solid",
+        borderColor: props.mode === `dark` ? `white` : `black`,
+       
     }
+
+
 
     
     return (
         <>
-        <h1 text-light className='container'>About Us</h1>
-        <div className='container' style={myStyle}>
-            <div id="accordion" style={myStyle}>
-                <div className="card">
-                    <div className="card-header" id="headingOne">
-                        <h5 className="mb-0">
-                            <button className="btn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Collapsible Group Item #1
+        <h1 text-light className={`container text-${props.mode === `light` ? `dark` : `light`}`}>About Us</h1>
+        <div className='container my-3'>
+            <div id="accordion">
+                <div className="card mx-3 my-3">
+                    <div className="card-header" id="headingOne" style={myStyle}>
+                        <h5 className="mb-0 my-3">
+                            <button className={`btn text-${props.mode === `light` ? `dark` : `light`}`} data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                About your Text Analyser
                             </button>
                         </h5>
                     </div>
 
-                    <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div id="collapseOne" className="collapse show my-3" aria-labelledby="headingOne" data-parent="#accordion">
                         <div className="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        <h4 className='text-muted lead'>This text manipulation tool offers a suite of basic text formatting functions. 
+                        Convert text to uppercase or lowercase, copy it to your clipboard for easy transfer, 
+                        remove unnecessary extra spaces, or clear the input field to start anew. 
+                        Ideal for quick text adjustments and simple text formatting tasks.</h4>
                         </div>
                     </div>
                 </div>
                 
                 
             </div>
-            <div className='container my-3'>
-                <button type="button" className="btn btn-secondary my-3" onClick={handleDarkMode}>{btnText}</button>
-            </div>
+            
         </div>
         </>
     )
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+            {/* <a className="navbar-brand" href="/">TextUtils</a> */}
             <Link className="navbar-brand" to="/">TextUtils</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -13,9 +14,11 @@ export default function Navbar(props) {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
+                        {/* <a className="nav-link" href="">{props.title} <span className="sr-only"></span></a> */}
                         <Link className="nav-link" to="/">{props.title} <span className="sr-only"></span></Link>
                     </li>
                     <li className="nav-item">
+                        {/* <a className="nav-link" href="/about">{props.randomShit}</a> */}
                         <Link className="nav-link" to="/about">{props.randomShit}</Link>
                     </li>
                 </ul>
